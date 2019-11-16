39d2e30f2b53099fbcd7482a2614609c55563af2
Tim Schmidt <tim.schmidt@ewe.net>
2019-11-16 12:07:39 +0100

    updated config-sample.py

--- config-sample.py
+++ config-sample.py
@@ -1,9 +1,9 @@
 # Required settings (must be changed)
-BOT_TOKEN               = "INSERT BOT TOKEN HERE"
+BOT_TOKEN               = "INSERT DM TOKEN HERE"
 
 # Optional settings (may be changed)
 BOT_DESCRIPTION         = "Fear and Terror Ticket Bot"
-BOT_CMD_PREFIX          = "#"
-BOT_MANAGER_ROLE        = 645021474402467842
-BOT_ROLE_CHANNELS       = [603283885442334758]
-BOT_DM_CHANNELS         = [620778714380238852]
+BOT_CMD_PREFIX          = "+"
+BOT_TICKET_MANAGER_ROLE = 521084603457470485 # GEN
+BOT_TICKET_CATEGORY     = 605830397510680596 # Bot Info
+BOT_TICKET_MAX_PER_USER = 1
